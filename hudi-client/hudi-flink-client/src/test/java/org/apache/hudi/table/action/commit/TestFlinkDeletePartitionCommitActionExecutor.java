743baab786ec14fc4931bcb7de5f8e5c46e3ae80
hudi <1817802738@qq.com>
2022-12-02 00:06:20 -0800

    init env

--- hudi-client/hudi-flink-client/src/test/java/org/apache/hudi/table/action/commit/TestFlinkDeletePartitionCommitActionExecutor.java
+++ hudi-client/hudi-flink-client/src/test/java/org/apache/hudi/table/action/commit/TestFlinkDeletePartitionCommitActionExecutor.java
@@ -6,6 +6,11 @@ import org.apache.hudi.common.model.HoodieTableType;
 import org.apache.hudi.common.model.OverwriteWithLatestAvroPayload;
 import org.apache.hudi.common.table.HoodieTableConfig;
 import org.apache.hudi.common.table.HoodieTableMetaClient;
+import org.apache.hudi.common.table.view.FileSystemViewStorageConfig;
+import org.apache.hudi.config.HoodieCompactionConfig;
+import org.apache.hudi.config.HoodieIndexConfig;
+import org.apache.hudi.config.HoodieWriteConfig;
+import org.apache.hudi.index.HoodieIndex;
 import org.apache.hudi.keygen.SimpleAvroKeyGenerator;
 import org.apache.hudi.testutils.HoodieFlinkClientTestHarness;
 import org.junit.jupiter.api.AfterEach;
@@ -16,6 +21,7 @@ import org.junit.jupiter.api.io.TempDir;
 import java.io.IOException;
 import java.util.Properties;
 
+import static org.apache.hudi.common.testutils.HoodieTestDataGenerator.TRIP_EXAMPLE_SCHEMA;
 import static org.apache.hudi.common.testutils.HoodieTestUtils.RAW_TRIPS_TEST_NAME;
 
 
@@ -27,6 +33,10 @@ public class TestFlinkDeletePartitionCommitActionExecutor extends HoodieFlinkCli
 
     public Configuration hadoopConf(){
         return context.getHadoopConf().get();
+    }
+
+    public String basePath(){
+        return basePath.toString();
     }
 
     @BeforeEach
@@ -44,11 +54,43 @@ public class TestFlinkDeletePartitionCommitActionExecutor extends HoodieFlinkCli
         Properties properties = populateMetaFile ? new Properties() : getPropertiesForKeyGen();
         properties.setProperty(HoodieTableConfig.BASE_FILE_FORMAT.key(), fileFormat.toString());
         HoodieTableMetaClient metaClient = getHoodieMetaClient(properties,HoodieTableType.COPY_ON_WRITE);
+        HoodieWriteConfig.Builder cfg = null;
     }
 
 
     @AfterEach
     public void cleanUp(){
+
+    }
+
+    protected HoodieWriteConfig.Builder getConfigBuilder(Boolean autoCommit) {
+        return getConfigBuilder(autoCommit, HoodieIndex.IndexType.BUCKET);
+    }
+
+    protected HoodieWriteConfig.Builder getConfigBuilder(Boolean autoCommit, HoodieIndex.IndexType hoodieIndex){
+        // delete operator can execute shuffle
+
+        HoodieWriteConfig.Builder writeConfig = HoodieWriteConfig.newBuilder().withPath(basePath())
+                .withSchema(TRIP_EXAMPLE_SCHEMA)
+                .withParallelism(2, 2)
+                .withDeleteParallelism(2)
+                .withAutoCommit(autoCommit)
+                .withCompactionConfig(HoodieCompactionConfig.newBuilder()
+                        .compactionSmallFileSize(1024 * 1024 * 1024L)
+                        // compact after write execute, can exists write latenry
+                        .withInlineCompaction(false).withMaxNumDeltaCommitsBeforeCompaction(1)
+                        //FIXME When rewriting data, preserves existing hoodie_commit_time
+                        .withPreserveCommitMetadata(false)
+                        .build())
+                // enable timeline
+        .withEmbeddedTimelineServerEnabled(true)
+        .withFileSystemViewConfig(new FileSystemViewStorageConfig.Builder()
+        .withRemoteServerPort(9091)
+        .withEnableBackupForRemoteFileSystemView(false).build())
+        .withIndexConfig(HoodieIndexConfig.newBuilder().withIndexType(hoodieIndex).build());
+
+       //ClusteringConfig RollbakUsingMarkers
+        return writeConfig;
 
     }
 
